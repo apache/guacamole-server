b203cf1fd2044ad94191b9b6a665eecea45cd75e
Michael Jumper <mike.jumper@guac-dev.org>
2014-04-01 23:40:00 -0700

    GUAC-584: Remove old free() - no longer necessary as buffer is static.

--- src/protocols/rdp/guac_handlers.c
+++ src/protocols/rdp/guac_handlers.c
@@ -92,7 +92,6 @@ int rdp_guac_client_free_handler(guac_client* client) {
     /* Free client data */
     cairo_surface_destroy(guac_client_data->opaque_glyph_surface);
     cairo_surface_destroy(guac_client_data->trans_glyph_surface);
-    free(guac_client_data->clipboard);
     free(guac_client_data);
 
     return 0;
