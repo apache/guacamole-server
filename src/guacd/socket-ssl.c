c5fd72bb98769fc06ea5779cc7c3c8579dbf0467
Michael Jumper <mike.jumper@guac-dev.org>
2016-02-01 15:12:47 -0800

    GUAC-1477: Ensure any fd_set used by guac_socket is always properly initialized.

--- src/guacd/socket-ssl.c
+++ src/guacd/socket-ssl.c
@@ -77,6 +77,10 @@ static int __guac_socket_ssl_select_handler(guac_socket* socket, int usec_timeou
     struct timeval timeout;
     int retval;
 
+    /* Initialize fd_set with single underlying file descriptor */
+    FD_ZERO(&fds);
+    FD_SET(data->fd, &fds);
+
     /* No timeout if usec_timeout is negative */
     if (usec_timeout < 0)
         retval = select(data->fd + 1, &fds, NULL, NULL, NULL); 
@@ -85,10 +89,6 @@ static int __guac_socket_ssl_select_handler(guac_socket* socket, int usec_timeou
     else {
         timeout.tv_sec = usec_timeout/1000000;
         timeout.tv_usec = usec_timeout%1000000;
-
-        FD_ZERO(&fds);
-        FD_SET(data->fd, &fds);
-
         retval = select(data->fd + 1, &fds, NULL, NULL, &timeout);
     }
 
