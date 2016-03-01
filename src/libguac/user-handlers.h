1726af0c9727ede735c3a03118653481ee43cdb2
Michael Jumper <mike.jumper@guac-dev.org>
2016-03-01 12:19:52 -0800

    GUAC-1389: Add missing parameter documentation for user-level instruction handlers.

--- src/libguac/user-handlers.h
+++ src/libguac/user-handlers.h
@@ -38,7 +38,22 @@
 #include "timestamp.h"
 
 /**
- * Internal handler for Guacamole instructions.
+ * Internal handler for Guacamole instructions. Instruction handlers will be
+ * invoked when their corresponding instructions are received. The mapping
+ * of instruction opcode to handler is defined by the
+ * __guac_instruction_handler_map array.
+ *
+ * @param user
+ *     The user that sent the instruction.
+ *
+ * @param argc
+ *     The number of arguments in argv.
+ *
+ * @param argv
+ *     The arguments included with the instruction, excluding the opcode.
+ *
+ * @return
+ *     Zero if the instruction was successfully handled, non-zero otherwise.
  */
 typedef int __guac_instruction_handler(guac_user* user, int argc, char** argv);
 
@@ -64,91 +79,92 @@ typedef struct __guac_instruction_handler_mapping {
  * is received, this handler will be called. Sync instructions are automatically
  * handled, thus there is no client handler for sync instruction.
  */
-int __guac_handle_sync(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_sync;
 
 /**
  * Internal initial handler for the mouse instruction. When a mouse instruction
  * is received, this handler will be called. The client's mouse handler will
  * be invoked if defined.
  */
-int __guac_handle_mouse(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_mouse;
 
 /**
  * Internal initial handler for the key instruction. When a key instruction
  * is received, this handler will be called. The client's key handler will
  * be invoked if defined.
  */
-int __guac_handle_key(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_key;
 
 /**
- * Internal initial handler for the clipboard instruction. When a clipboard instruction
- * is received, this handler will be called. The client's clipboard handler will
- * be invoked if defined.
+ * Internal initial handler for the clipboard instruction. When a clipboard
+ * instruction is received, this handler will be called. The client's clipboard
+ * handler will be invoked if defined.
  */
-int __guac_handle_clipboard(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_clipboard;
 
 /**
  * Internal initial handler for the file instruction. When a file instruction
  * is received, this handler will be called. The client's file handler will
  * be invoked if defined.
  */
-int __guac_handle_file(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_file;
 
 /**
  * Internal initial handler for the pipe instruction. When a pipe instruction
  * is received, this handler will be called. The client's pipe handler will
  * be invoked if defined.
  */
-int __guac_handle_pipe(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_pipe;
 
 /**
  * Internal initial handler for the ack instruction. When a ack instruction
  * is received, this handler will be called. The client's ack handler will
  * be invoked if defined.
  */
-int __guac_handle_ack(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_ack;
 
 /**
  * Internal initial handler for the blob instruction. When a blob instruction
  * is received, this handler will be called. The client's blob handler will
  * be invoked if defined.
  */
-int __guac_handle_blob(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_blob;
 
 /**
  * Internal initial handler for the end instruction. When a end instruction
  * is received, this handler will be called. The client's end handler will
  * be invoked if defined.
  */
-int __guac_handle_end(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_end;
 
 /**
  * Internal initial handler for the get instruction. When a get instruction
  * is received, this handler will be called. The client's get handler will
  * be invoked if defined.
  */
-int __guac_handle_get(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_get;
 
 /**
  * Internal initial handler for the put instruction. When a put instruction
  * is received, this handler will be called. The client's put handler will
  * be invoked if defined.
  */
-int __guac_handle_put(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_put;
 
 /**
  * Internal initial handler for the size instruction. When a size instruction
  * is received, this handler will be called. The client's size handler will
  * be invoked if defined.
  */
-int __guac_handle_size(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_size;
 
 /**
- * Internal initial handler for the disconnect instruction. When a disconnect instruction
- * is received, this handler will be called. Disconnect instructions are automatically
- * handled, thus there is no client handler for disconnect instruction.
+ * Internal initial handler for the disconnect instruction. When a disconnect
+ * instruction is received, this handler will be called. Disconnect
+ * instructions are automatically handled, thus there is no client handler for
+ * disconnect instruction.
  */
-int __guac_handle_disconnect(guac_user* user, int argc, char** argv);
+__guac_instruction_handler __guac_handle_disconnect;
 
 /**
  * Instruction handler mapping table. This is a NULL-terminated array of
