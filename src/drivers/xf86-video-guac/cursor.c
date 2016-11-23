c5254b864f3b563613e529496a70e21f0cd54f32
Michael Jumper <mjumper@apache.org>
2016-11-22 16:47:31 -0800

    GUACAMOLE-168: Actually implement true cursor support.

--- src/drivers/xf86-video-guac/cursor.c
+++ src/drivers/xf86-video-guac/cursor.c
@@ -19,23 +19,131 @@
  */
 
 #include "config.h"
+#include "cursor.h"
+#include "display.h"
+#include "screen.h"
 
 #include <xorg-server.h>
 #include <xf86.h>
 #include <xf86Cursor.h>
 #include <cursorstr.h>
 
+static void guac_drv_cursor_set_argb(guac_drv_cursor* guac_cursor,
+        CursorPtr cursor) {
+
+    xf86Msg(X_INFO, "guac: STUB: %s (%i, %i) %ix%i\n", __func__,
+            guac_cursor->hotspot_x, guac_cursor->hotspot_y,
+            guac_cursor->width, guac_cursor->height);
+
+}
+
+static void guac_drv_cursor_set_glyph(guac_drv_cursor* guac_cursor,
+        CursorPtr cursor) {
+
+    int x;
+    int y;
+
+    /* Calculate foreground color */
+    uint32_t fg = 0xFF000000 | (cursor->foreRed << 16)
+        | (cursor->foreGreen << 8) | (cursor->foreBlue);
+
+    /* Calculate background color */
+    uint32_t bg = 0xFF000000 | (cursor->backRed << 16)
+        | (cursor->backGreen << 8) | (cursor->backBlue);
+
+    /* Get source and destination image data */
+    uint32_t* src_row = (uint32_t*) cursor->bits->source;
+    uint32_t* mask_row = (uint32_t*) cursor->bits->mask;
+    uint32_t* dst_row = guac_cursor->image;
+
+    /* For each row of image data */
+    for (y = 0; y < guac_cursor->height; y++) {
+
+        /* Get first pixel/bit in current row */
+        uint32_t* dst = dst_row;
+        uint32_t src = *src_row;
+        uint32_t mask = *mask_row;
+
+        /* For each pixel within row */
+        for (x = 0; x < guac_cursor->width; x++) {
+
+            /* Draw pixel only if mask is set. */
+            if (mask & 0x1) {
+
+                /* Select foreground/background depending on source bit */
+                *dst = (src & 0x1) ? fg : bg;
+
+            }
+
+            /* Next pixel */
+            dst++;
+            src >>= 1;
+            mask >>= 1;
+
+        }
+
+        /* Next row */
+        dst_row += GUAC_DRV_CURSOR_MAX_WIDTH;
+        src_row++;
+        mask_row++;
+
+    }
+
+}
+
+static unsigned char* guac_drv_realize_cursor(xf86CursorInfoPtr cursor_info,
+        CursorPtr cursor) {
+
+    guac_drv_cursor* guac_cursor =
+        (guac_drv_cursor*) calloc(1, sizeof(guac_drv_cursor));
+
+    /* Assign dimensions */
+    guac_cursor->width = cursor->bits->width;
+    guac_cursor->height = cursor->bits->height;
+
+    /* Assign hotspot */
+    guac_cursor->hotspot_x = cursor->bits->xhot;
+    guac_cursor->hotspot_y = cursor->bits->yhot;
+
+    /* Use ARGB cursor image if available */
+    if (cursor->bits->argb != NULL)
+        guac_drv_cursor_set_argb(guac_cursor, cursor);
+
+    /* Otherwise, use glyph cursor */
+    else
+        guac_drv_cursor_set_glyph(guac_cursor, cursor);
+
+    return (unsigned char*) guac_cursor;
+
+}
+
 static void guac_drv_set_cursor_colors(ScrnInfoPtr screen, int bg, int fg) {
-    xf86Msg(X_INFO, "guac: STUB: %s\n", __func__);
+    /* Do nothing */
 }
 
 static void guac_drv_set_cursor_position(ScrnInfoPtr screen, int x, int y) {
-    /*xf86Msg(X_INFO, "guac: STUB: %s\n", __func__);*/
+    /* Do nothing */
 }
 
-static void guac_drv_load_cursor_image(ScrnInfoPtr screen,
+static void guac_drv_load_cursor_image(ScrnInfoPtr screen_info,
         unsigned char* image) {
-    xf86Msg(X_INFO, "guac: STUB: %s\n", __func__);
+
+    guac_drv_cursor* guac_cursor = (guac_drv_cursor*) image;
+
+    /* Get guac_drv_screen */
+    ScreenPtr screen = screen_info->pScreen;
+    guac_drv_screen* guac_screen =
+        (guac_drv_screen*) dixGetPrivate(&(screen->devPrivates),
+                GUAC_SCREEN_PRIVATE);
+
+    /* Set cursor of display */
+    guac_common_cursor_set_argb(guac_screen->display->display->cursor,
+            guac_cursor->hotspot_x, guac_cursor->hotspot_y,
+            (unsigned char*) guac_cursor->image, guac_cursor->width,
+            guac_cursor->height, GUAC_DRV_CURSOR_STRIDE);
+
+    guac_drv_display_touch(guac_screen->display);
+
 }
 
 static void guac_drv_hide_cursor(ScrnInfoPtr screen) {
@@ -50,19 +158,6 @@ static Bool guac_drv_use_hw_cursor(ScreenPtr screen, CursorPtr cursor) {
     return TRUE;
 }
 
-static Bool guac_drv_use_hw_cursor_argb(ScreenPtr screen, CursorPtr cursor) {
-    return TRUE;
-}
-
-static void guac_drv_load_cursor_argb(ScrnInfoPtr screen_info,
-        CursorPtr cursor) {
-    xf86Msg(X_INFO, "guac: STUB: %s: (%i, %i) %ix%i\n", __func__,
-            cursor->bits->xhot,
-            cursor->bits->yhot,
-            cursor->bits->width,
-            cursor->bits->height);
-}
-
 Bool guac_drv_init_cursor(ScreenPtr screen) {
 
     /* Get cursor info struct */
@@ -71,25 +166,24 @@ Bool guac_drv_init_cursor(ScreenPtr screen) {
         return FALSE;
 
     /* Init cursor info */
-    cursor_info->MaxHeight = 64;
-    cursor_info->MaxWidth = 64;
+    cursor_info->MaxHeight = GUAC_DRV_CURSOR_MAX_WIDTH;
+    cursor_info->MaxWidth = GUAC_DRV_CURSOR_MAX_HEIGHT;
     cursor_info->Flags =
           HARDWARE_CURSOR_ARGB
-        | HARDWARE_CURSOR_UPDATE_UNHIDDEN;
+        | HARDWARE_CURSOR_UPDATE_UNHIDDEN
+        | HARDWARE_CURSOR_SOURCE_MASK_INTERLEAVE_1;
 
     /* Set handlers */
+    cursor_info->RealizeCursor = guac_drv_realize_cursor;
     cursor_info->SetCursorPosition = guac_drv_set_cursor_position;
     cursor_info->HideCursor = guac_drv_hide_cursor;
     cursor_info->ShowCursor = guac_drv_show_cursor;
 
-    /* Legacy cursors */
+    /* Glyph cursors (ARGB data is stored within the cursor data by our
+     * implementation of RealizeCursor) */
     cursor_info->SetCursorColors = guac_drv_set_cursor_colors;
     cursor_info->UseHWCursor = guac_drv_use_hw_cursor;
     cursor_info->LoadCursorImage = guac_drv_load_cursor_image;
-
-    /* Full ARGB cursors */
-    cursor_info->UseHWCursorARGB = guac_drv_use_hw_cursor_argb;
-    cursor_info->LoadCursorARGB = guac_drv_load_cursor_argb;
 
     return xf86InitCursor(screen, cursor_info);
 
--- src/drivers/xf86-video-guac/cursor.h
+++ src/drivers/xf86-video-guac/cursor.h
@@ -27,6 +27,56 @@
 #include <xf86.h>
 
 /**
+ * The maximum width of a cursor supported by this driver, in pixels.
+ */
+#define GUAC_DRV_CURSOR_MAX_WIDTH 64
+
+/**
+ * The maximum height of a cursor supported by this driver, in pixels.
+ */
+#define GUAC_DRV_CURSOR_MAX_HEIGHT 64
+
+/**
+ * The number of bytes in each row of ARGB image data stored within a
+ * guac_drv_cursor.
+ */
+#define GUAC_DRV_CURSOR_STRIDE (GUAC_DRV_CURSOR_MAX_HEIGHT * 4)
+
+/**
+ * A single ARGB mouse cursor and corresponding metadata.
+ */
+typedef struct guac_drv_cursor {
+
+    /**
+     * The raw ARGB image data of this cursor. Each row of image data is
+     * GUAC_DRV_CURSOR_STRIDE bytes long, made up of 32-bit ARGB pixels. All
+     * pixels are set to transparent black by default.
+     */
+    uint32_t image[GUAC_DRV_CURSOR_MAX_WIDTH * GUAC_DRV_CURSOR_MAX_HEIGHT];
+
+    /**
+     * The X coordinate of the mouse cursor's hotspot.
+     */
+    int hotspot_x;
+
+    /**
+     * The Y coordinate of the mouse cursor's hotspot.
+     */
+    int hotspot_y;
+
+    /**
+     * The width of the mouse cursor, in pixels.
+     */
+    int width;
+
+    /**
+     * The height of the mouse cursor, in pixels.
+     */
+    int height;
+
+} guac_drv_cursor;
+
+/**
  * Initialize hardware cursor rendering.
  */
 Bool guac_drv_init_cursor(ScreenPtr screen);
