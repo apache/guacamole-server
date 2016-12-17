8d969bf32ecc3f5f27224eab06044a211ee57e8f
Michael Jumper <mjumper@apache.org>
2016-12-17 00:29:30 -0800

    GUACAMOLE-168: Use X authorization when connecting as a client.

--- src/drivers/xf86-video-guac/xclient.c
+++ src/drivers/xf86-video-guac/xclient.c
@@ -26,23 +26,80 @@
 
 #include <xcb/xcb.h>
 
+/**
+ * Populates the given xcb_auth_info_t structure with valid X authorization
+ * data.
+ *
+ * @return
+ *     Zero on success, non-zero if X authorization data could not be
+ *     created.
+ */
+static int guac_drv_authorize(xcb_auth_info_t* auth) {
+
+    /* FIXME: Generate and add authorization, rather than searching */
+
+    int id;
+
+    /* Loop through all fake client IDs */
+    for (id = 0; id < 1024; id++) {
+
+        unsigned short name_len;
+        const char* name;
+
+        unsigned short data_len;
+        char* data;
+
+        /* Attempt to read authorization for (fake) client */
+        if (AuthorizationFromID(id, &name_len, &name, &data_len, &data)) {
+
+            /* Copy name */
+            auth->namelen = name_len;
+            auth->name = strdup(name);
+
+            /* Copy data */
+            auth->datalen = data_len;
+            auth->data = malloc(data_len);
+            memcpy(auth->data, data, data_len);
+
+            /* Authorization successfully retrieved */
+            return 0;
+
+        }
+
+    }
+
+    /* Authorization could not be read */
+    return 1;
+
+}
+
 xcb_connection_t* guac_drv_get_connection() {
 
     char display_name[64];
+
+    /* Attempt to read X authorization */
+    xcb_auth_info_t auth;
+    if (guac_drv_authorize(&auth))
+        return NULL;
 
     /* Get display name of display served by Guacamole X.Org driver */
     sprintf(display_name, ":%s", display);
 
     /* Connect to X server hosting display */
-    xcb_connection_t* connection = xcb_connect(display_name, NULL);
+    xcb_connection_t* connection = xcb_connect_to_display_with_auth_info(
+            display_name, &auth, NULL);
 
     /* Return NULL if connection fails */
     if (xcb_connection_has_error(connection)) {
         xcb_disconnect(connection);
+        free(auth.name);
+        free(auth.data);
         return NULL;
     }
 
     /* Connection succeeded */
+    free(auth.name);
+    free(auth.data);
     return connection;
 
 }
