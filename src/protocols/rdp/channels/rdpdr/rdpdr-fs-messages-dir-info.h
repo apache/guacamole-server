fbb759ab820fe31a69aad8ccd0caed3a93d40932
Michael Jumper <mjumper@apache.org>
2019-12-28 12:01:44 -0800

    GUACAMOLE-249: Correct mismatched ifndef/define header include check.

--- src/protocols/rdp/channels/rdpdr/rdpdr-fs-messages-dir-info.h
+++ src/protocols/rdp/channels/rdpdr/rdpdr-fs-messages-dir-info.h
@@ -18,7 +18,7 @@
  */
 
 #ifndef GUAC_RDP_CHANNELS_RDPDR_FS_MESSAGES_DIR_INFO_H
-#define RDP_CHANNELS_GUAC_RDPDR_FS_MESSAGES_DIR_INFO_H
+#define GUAC_RDP_CHANNELS_RDPDR_FS_MESSAGES_DIR_INFO_H
 
 /**
  * Handlers for directory queries received over the RDPDR channel via the
