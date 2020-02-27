b85fff7c0868fd87d0a6f0b8e31257c627e992e0
mather <mather314@gmail.com>
2020-02-27 10:29:05 +0900

    GUACAMOLE-951: Fix condition for UINT32

--- src/protocols/rdp/settings.c
+++ src/protocols/rdp/settings.c
@@ -1189,13 +1189,13 @@ void guac_rdp_push_settings(guac_client* client,
     rdp_settings->DesktopHeight = guac_settings->height;
     rdp_settings->AlternateShell = guac_rdp_strdup(guac_settings->initial_program);
     rdp_settings->KeyboardLayout = guac_settings->server_layout->freerdp_keyboard_layout;
-    if (guac_settings->server_layout->freerdp_keyboard_type > 0) {
+    if (guac_settings->server_layout->freerdp_keyboard_type != 0) {
         rdp_settings->KeyboardType = guac_settings->server_layout->freerdp_keyboard_type;
     }
-    if (guac_settings->server_layout->freerdp_keyboard_subtype > 0) {
+    if (guac_settings->server_layout->freerdp_keyboard_subtype != 0) {
         rdp_settings->KeyboardSubType = guac_settings->server_layout->freerdp_keyboard_subtype;
     }
-    if (guac_settings->server_layout->freerdp_keyboard_function_key > 0) {
+    if (guac_settings->server_layout->freerdp_keyboard_function_key != 0) {
         rdp_settings->KeyboardFunctionKey = guac_settings->server_layout->freerdp_keyboard_function_key;
     }
 
