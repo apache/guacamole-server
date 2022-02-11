e6c1c2b12d5a6e527d32322fe44b3cd4a052791b
bookread-he <54766287+bookread-he@users.noreply.github.com>
2022-02-11 15:51:35 +0800

    Update sftp.c

--- src/common-ssh/sftp.c
+++ src/common-ssh/sftp.c
@@ -660,19 +660,19 @@ static int guac_common_ssh_sftp_ls_ack_handler(guac_user* user,
 
         //adding file size and permission
         char tmpstr[150];
-		if (LIBSSH2_SFTP_S_ISDIR(attributes.permissions)) {
-			//mimetype = GUAC_USER_STREAM_INDEX_MIMETYPE;
-			sprintf(tmpstr, "{\"mime\":\"%s\",\"size\":%llu,\"perm\":%lu}",
-				GUAC_USER_STREAM_INDEX_MIMETYPE, attributes.filesize,
-				attributes.permissions);
-		}
-		else {
-			//mimetype = "application/octet-stream";
-			sprintf(tmpstr, "{\"mime\":\"%s\",\"size\":%llu,\"perm\":%lu}",
-				"application/octet-stream", attributes.filesize,
-				attributes.permissions);
-		}
-		mimetype = tmpstr;
+	if (LIBSSH2_SFTP_S_ISDIR(attributes.permissions)) {
+		//mimetype = GUAC_USER_STREAM_INDEX_MIMETYPE;
+		sprintf(tmpstr, "{\"mime\":\"%s\",\"size\":%llu,\"perm\":%lu}",
+			GUAC_USER_STREAM_INDEX_MIMETYPE, attributes.filesize,
+			attributes.permissions);
+	}
+	else {
+		//mimetype = "application/octet-stream";
+		sprintf(tmpstr, "{\"mime\":\"%s\",\"size\":%llu,\"perm\":%lu}",
+			"application/octet-stream", attributes.filesize,
+			attributes.permissions);
+	}
+	mimetype = tmpstr;
 
         /* Write entry, waiting for next ack if a blob is written */
         if (guac_common_json_write_property(user, stream,
