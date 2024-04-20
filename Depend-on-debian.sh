f8b1cf97ee991410235ec26dc042d353aa1fd14d
CooperDActor <136168153+CooperDActor@users.noreply.github.com>
2024-04-20 11:09:55 +1000

    Create Depend-on-debian.sh

---
+++ Depend-on-debian.sh
@@ -0,0 +1,32 @@
+# FFmpeg
+sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
+
+# FreeRDP
+sudo apt-get install -y freerdp2-dev
+
+# Pango
+sudo apt-get install -y libpango1.0-dev
+
+# libssh2
+sudo apt-get install -y libssh2-1-dev
+
+# libtelnet
+sudo apt-get install -y libtelnet-dev
+
+# libVNCServer
+sudo apt-get install -y libvncserver-dev
+
+# libwebsockets
+sudo apt-get install -y libwebsockets-dev
+
+# PulseAudio
+sudo apt-get install -y libpulse-dev
+
+# OpenSSL
+sudo apt-get install -y libssl-dev
+
+# libvorbis
+sudo apt-get install -y libvorbis-dev
+
+# libwebp
+sudo apt-get install -y libwebp-dev
