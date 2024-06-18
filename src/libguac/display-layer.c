94b50b1907660c23f3b476f6556bb521ee452a19
Michael Jumper <mjumper@apache.org>
2024-06-18 16:41:54 -0700

    GUACAMOLE-377: Flush an automatic frame for mouse changes only if there are no other graphical changes pending.

--- src/libguac/display-layer.c
+++ src/libguac/display-layer.c
@@ -25,6 +25,24 @@
 #include <cairo/cairo.h>
 #include <stdint.h>
 
+/**
+ * Notifies the display associated with the given layer that the given layer
+ * has been modified in some way for the current pending frame. If the layer is
+ * not the cursor layer, the pending_frame_dirty_excluding_mouse flag of the
+ * display is updated accordingly.
+ *
+ * @param layer
+ *     The layer that was modified.
+ */
+static void PFW_guac_display_layer_touch(guac_display_layer* layer) {
+
+    guac_display* display = layer->display;
+
+    if (layer != display->cursor_buffer)
+        display->pending_frame_dirty_excluding_mouse = 1;
+
+}
+
 void guac_display_layer_get_bounds(guac_display_layer* layer, guac_rect* bounds) {
 
     guac_display* display = layer->display;
@@ -48,6 +66,7 @@ void guac_display_layer_move(guac_display_layer* layer, int x, int y) {
 
     layer->pending_frame.x = x;
     layer->pending_frame.y = y;
+    PFW_guac_display_layer_touch(layer);
 
     guac_rwlock_release_lock(&display->pending_frame.lock);
 
@@ -59,6 +78,7 @@ void guac_display_layer_stack(guac_display_layer* layer, int z) {
     guac_rwlock_acquire_write_lock(&display->pending_frame.lock);
 
     layer->pending_frame.z = z;
+    PFW_guac_display_layer_touch(layer);
 
     guac_rwlock_release_lock(&display->pending_frame.lock);
 
@@ -70,6 +90,7 @@ void guac_display_layer_set_parent(guac_display_layer* layer, const guac_display
     guac_rwlock_acquire_write_lock(&display->pending_frame.lock);
 
     layer->pending_frame.parent = parent->layer;
+    PFW_guac_display_layer_touch(layer);
 
     guac_rwlock_release_lock(&display->pending_frame.lock);
 
@@ -81,6 +102,7 @@ void guac_display_layer_set_opacity(guac_display_layer* layer, int opacity) {
     guac_rwlock_acquire_write_lock(&display->pending_frame.lock);
 
     layer->pending_frame.opacity = opacity;
+    PFW_guac_display_layer_touch(layer);
 
     guac_rwlock_release_lock(&display->pending_frame.lock);
 
@@ -92,6 +114,7 @@ void guac_display_layer_set_lossless(guac_display_layer* layer, int lossless) {
     guac_rwlock_acquire_write_lock(&display->pending_frame.lock);
 
     layer->pending_frame.lossless = lossless;
+    PFW_guac_display_layer_touch(layer);
 
     guac_rwlock_release_lock(&display->pending_frame.lock);
 
@@ -103,6 +126,7 @@ void guac_display_layer_set_multitouch(guac_display_layer* layer, int touches) {
     guac_rwlock_acquire_write_lock(&display->pending_frame.lock);
 
     layer->pending_frame.touches = touches;
+    PFW_guac_display_layer_touch(layer);
 
     guac_rwlock_release_lock(&display->pending_frame.lock);
 
@@ -115,6 +139,7 @@ void guac_display_layer_resize(guac_display_layer* layer, int width, int height)
     guac_rwlock_acquire_write_lock(&display->last_frame.lock);
 
     PFW_LFW_guac_display_layer_resize(layer, width, height);
+    PFW_guac_display_layer_touch(layer);
 
     guac_rwlock_release_lock(&display->last_frame.lock);
     guac_rwlock_release_lock(&display->pending_frame.lock);
@@ -197,6 +222,7 @@ void guac_display_layer_close_raw(guac_display_layer* layer, guac_display_layer_
     guac_display* display = layer->display;
 
     guac_rect_extend(&layer->pending_frame.dirty, &context->dirty);
+    PFW_guac_display_layer_touch(layer);
 
     guac_rwlock_release_lock(&display->pending_frame.lock);
 
@@ -237,7 +263,10 @@ guac_display_layer_cairo_context* guac_display_layer_open_cairo(guac_display_lay
 void guac_display_layer_close_cairo(guac_display_layer* layer, guac_display_layer_cairo_context* context) {
 
     guac_display* display = layer->display;
+
     guac_rect_extend(&layer->pending_frame.dirty, &context->dirty);
+    PFW_guac_display_layer_touch(layer);
+
     guac_rwlock_release_lock(&display->pending_frame.lock);
 
 }
