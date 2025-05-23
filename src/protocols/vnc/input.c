d44d527e091dd58a3e4c09422036d107061c53aa
Corentin SORIANO <csoriano@apache.org>
2025-05-23 09:46:16 +0200

    GUACAMOLE-288: Allow windows to have different dimensions.

--- src/protocols/vnc/input.c
+++ src/protocols/vnc/input.c
@@ -65,7 +65,8 @@ int guac_vnc_user_key_handler(guac_user* user, int keysym, int pressed) {
 }
 
 #ifdef LIBVNC_HAS_RESIZE_SUPPORT
-int guac_vnc_user_size_handler(guac_user* user, int width, int height, int monitors) {
+int guac_vnc_user_size_handler(guac_user* user, int width, int height,
+        int x_position, int top_offset) {
 
     guac_user_log(user, GUAC_LOG_TRACE, "Running user size handler.");
 
