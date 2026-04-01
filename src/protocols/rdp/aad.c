37b1d5a7afdc1715dcf4a02b79df0209a6b7d702
Alexander Leitner <aleitner@keepersecurity.com>
2026-04-01 17:13:08 -0400

    GUACAMOLE-2210: Update Chrome User-Agent version to 146 for AAD login requests.

--- src/protocols/rdp/aad.c
+++ src/protocols/rdp/aad.c
@@ -68,7 +68,7 @@
  */
 #define GUAC_AAD_USER_AGENT \
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " \
-    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    "(KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36"
 
 /**
  * HTTP response structure for AAD requests.
