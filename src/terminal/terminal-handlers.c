52aafa7ade4f786a0ed51d2b832b5ea34d0266e3
Andy Ward <andy.ward.uk@gmail.com>
2026-04-03 16:09:43 +0100

    GUACAMOLE-2213: Added missing brackets in comments

--- src/terminal/terminal-handlers.c
+++ src/terminal/terminal-handlers.c
@@ -1309,7 +1309,7 @@ int guac_terminal_set_directory(guac_terminal* term, unsigned char c) {
     static char filename[2048];
     static int length = 0;
 
-    /* Stop on ECMA-48 ST (String Terminator */
+    /* Stop on ECMA-48 ST (String Terminator) */
     if (c == 0x9C || c == 0x5C || c == 0x07) {
         filename[length++] = '\0';
         term->char_handler = guac_terminal_echo;
@@ -1334,7 +1334,7 @@ int guac_terminal_download(guac_terminal* term, unsigned char c) {
     static char filename[2048];
     static int length = 0;
 
-    /* Stop on ECMA-48 ST (String Terminator */
+    /* Stop on ECMA-48 ST (String Terminator) */
     if (c == 0x9C || c == 0x5C || c == 0x07) {
         filename[length++] = '\0';
         term->char_handler = guac_terminal_echo;
@@ -1456,7 +1456,7 @@ int guac_terminal_window_title(guac_terminal* term, unsigned char c) {
 
     guac_socket* socket = term->client->socket;
 
-    /* Stop on ECMA-48 ST (String Terminator */
+    /* Stop on ECMA-48 ST (String Terminator) */
     if (c == 0x9C || c == 0x5C || c == 0x07) {
 
         /* Terminate and reset stored title */
@@ -1552,7 +1552,7 @@ int guac_terminal_xterm_palette(guac_terminal* term, unsigned char c) {
 
     }
 
-    /* Stop on ECMA-48 ST (String Terminator */
+    /* Stop on ECMA-48 ST (String Terminator) */
     if (c == 0x9C || c == 0x5C || c == 0x07)
         term->char_handler = guac_terminal_echo;
 
@@ -1561,7 +1561,7 @@ int guac_terminal_xterm_palette(guac_terminal* term, unsigned char c) {
 }
 
 int guac_terminal_unknown_osc(guac_terminal* term, unsigned char c) {
-    /* Stop on ECMA-48 ST (String Terminator */
+    /* Stop on ECMA-48 ST (String Terminator) */
     if (c == 0x9C || c == 0x5C || c == 0x07)
         term->char_handler = guac_terminal_echo;
 
@@ -1616,7 +1616,7 @@ int guac_terminal_osc(guac_terminal* term, unsigned char c) {
 
     }
 
-    /* Stop on ECMA-48 ST (String Terminator */
+    /* Stop on ECMA-48 ST (String Terminator) */
     else if (c == 0x9C || c == 0x5C || c == 0x07)
         term->char_handler = guac_terminal_echo;
 
