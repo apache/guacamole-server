55f55caffb31a9bf88823c13d070c628ac29c68a
Virtually Nick <vnick@apache.org>
2026-07-05 15:10:24 -0400

    GUACAMOLE-2294: Reorganize config file reading to avoid resource leak.

--- src/guacd/conf-file.c
+++ src/guacd/conf-file.c
@@ -197,24 +197,21 @@ guacd_config* guacd_conf_load() {
 
     /* Read configuration from file */
     int fd = open(GUACD_CONF_FILE, O_RDONLY);
-    if (fd > 0) {
-
-        int retval = guacd_conf_parse_file(conf, fd);
-        close(fd);
-
-        if (retval != 0) {
-            fprintf(stderr, "Unable to parse \"" GUACD_CONF_FILE "\".\n");
-            guac_mem_free(conf->bind_host);
-            guac_mem_free(conf->bind_port);
-            guac_mem_free(conf);
-            return NULL;
-        }
-
-    }
 
     /* Notify of errors preventing reading */
-    else if (errno != ENOENT) {
+    if (fd < 0 && errno != ENOENT) {
         fprintf(stderr, "Unable to open \"" GUACD_CONF_FILE "\": %s\n", strerror(errno));
+        guac_mem_free(conf->bind_host);
+        guac_mem_free(conf->bind_port);
+        guac_mem_free(conf);
+        return NULL;
+    }
+
+    int retval = guacd_conf_parse_file(conf, fd);
+    close(fd);
+
+    if (retval != 0) {
+        fprintf(stderr, "Unable to parse \"" GUACD_CONF_FILE "\".\n");
         guac_mem_free(conf->bind_host);
         guac_mem_free(conf->bind_port);
         guac_mem_free(conf);
